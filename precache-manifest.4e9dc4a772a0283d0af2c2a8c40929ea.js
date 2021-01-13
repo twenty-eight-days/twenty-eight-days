301674328d82cafef37cc738c8e399f1ecca6130
Rahel Lüthy <rahel.luethy@gmail.com>
2021-01-13 18:05:31 +0100

    Updates

---
+++ precache-manifest.4e9dc4a772a0283d0af2c2a8c40929ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4bdf9bfcf78e47a5fae470e4041bfc",
+    "url": "/twenty-eight-days/index.html"
+  },
+  {
+    "revision": "4c6089d0c2ec10c53d87",
+    "url": "/twenty-eight-days/static/js/2.545d5b74.chunk.js"
+  },
+  {
+    "revision": "dfac9efc4f63b844b531f2c9d7c10191",
+    "url": "/twenty-eight-days/static/js/2.545d5b74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9468c0fa50de83b18e4",
+    "url": "/twenty-eight-days/static/js/main.7f07505b.chunk.js"
+  },
+  {
+    "revision": "3dbd7fc45e0e0186cdd8",
+    "url": "/twenty-eight-days/static/js/runtime-main.9fb7f1b1.js"
+  }
+]);

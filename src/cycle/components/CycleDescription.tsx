61804e6979884bc51cd89abb81848bc4778b186e
Rahel Lüthy <rahel.luethy@gmail.com>
2021-01-14 08:17:22 +0100

    #17: Improve expected next start date description

--- src/cycle/components/CycleDescription.tsx
+++ src/cycle/components/CycleDescription.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Typography } from '@material-ui/core'
 import { dateWithoutWeekDay, dateWithWeekDay } from '../utils'
 import addDays from 'date-fns/addDays'
+import { isBefore } from 'date-fns'
 
 interface Props {
   readonly currentDay: number
@@ -9,24 +10,36 @@ interface Props {
   readonly lastStartDate: number
 }
 
-export const CycleDescription = ({ currentDay, median, lastStartDate }: Props) => (
-  <div>
-    <Typography variant={'body1'} align={'center'}>
-      Hey – today is {dateWithWeekDay(new Date().valueOf())}
-    </Typography>
-    <Typography variant={'h1'} align={'center'}>
-      {currentDay}
-    </Typography>
-    <Typography variant={'body1'} align={'center'}>
-      days since your last period, which started on {dateWithoutWeekDay(lastStartDate)}.
-    </Typography>
-    <br />
-    <Typography variant={'body1'} align={'center'}>
-      Your next period is expected on <b>{dateWithWeekDay(addDays(lastStartDate, median).valueOf())}</b>.
-    </Typography>
-    <br />
-    <Typography variant={'body1'} align={'center'}>
-      Your <b>median cycle</b> length is {median} days.
-    </Typography>
-  </div>
-)
+export const CycleDescription = ({ currentDay, median, lastStartDate }: Props) => {
+  const expectedStartDate = addDays(lastStartDate, median - 1).valueOf()
+  const expectedStart = isBefore(expectedStartDate, new Date()) ? (
+    <b>{'today'}</b>
+  ) : (
+    <div>
+      {'on '}
+      <b>{dateWithWeekDay(expectedStartDate)}</b>
+    </div>
+  )
+
+  return (
+    <div>
+      <Typography variant={'body1'} align={'center'}>
+        Hey – today is {dateWithWeekDay(new Date().valueOf())}
+      </Typography>
+      <Typography variant={'h1'} align={'center'}>
+        {currentDay}
+      </Typography>
+      <Typography variant={'body1'} align={'center'}>
+        days since your last period, which started on {dateWithoutWeekDay(lastStartDate)}.
+      </Typography>
+      <br />
+      <Typography variant={'body1'} align={'center'}>
+        Your next period is expected {expectedStart}.
+      </Typography>
+      <br />
+      <Typography variant={'body1'} align={'center'}>
+        Your <b>median cycle</b> length is {median} days.
+      </Typography>
+    </div>
+  )
+}

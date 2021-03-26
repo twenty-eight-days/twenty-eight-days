34a48be94b8c25d4e11ac4ecafd971f8c9340284
Rahel Lüthy <rahel.luethy@gmail.com>
2021-03-26 18:08:05 +0100

    Updates

---
+++ precache-manifest.325418203e68cd01e2725cebb127581b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd56dacd15420e4ff4bf91f1915cc200",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b35b15283589abfcf36",
+    "url": "./static/js/2.7eb2fd61.chunk.js"
+  },
+  {
+    "revision": "dfac9efc4f63b844b531f2c9d7c10191",
+    "url": "./static/js/2.7eb2fd61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d2cb6c09569b9801c39",
+    "url": "./static/js/main.71456c8a.chunk.js"
+  },
+  {
+    "revision": "97af5b63f46ffa41a311",
+    "url": "./static/js/runtime-main.af3354b2.js"
+  }
+]);

3f0e05dac57639d6e8c6712232e8bfb182b7e872
Rahel Lüthy <rahel.luethy@gmail.com>
2021-04-24 08:20:07 +0200

    Updates

---
+++ precache-manifest.4d4e8b134e563ea854e9286a6f5f3edf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1e38f9dc105e82bf24b16072c419bf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2842486878b48dcfe5e1",
+    "url": "./static/js/2.5993c930.chunk.js"
+  },
+  {
+    "revision": "dfac9efc4f63b844b531f2c9d7c10191",
+    "url": "./static/js/2.5993c930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eafd0f385dbc26851bfb",
+    "url": "./static/js/main.80b59790.chunk.js"
+  },
+  {
+    "revision": "97af5b63f46ffa41a311",
+    "url": "./static/js/runtime-main.af3354b2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.325418203e68cd01e2725cebb127581b.js"
+  "./precache-manifest.4d4e8b134e563ea854e9286a6f5f3edf.js"
 );
 
 self.addEventListener('message', (event) => {
